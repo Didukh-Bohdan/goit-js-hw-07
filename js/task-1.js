643a847a4d104fdb400c15d67e802d4bde3d8e94
Didukh-Bohdan <bogdandidukh666@gmail.com>
2024-01-29 18:54:01 +0100

    load

---
+++ js/task-1.js
@@ -0,0 +1,6 @@
+const categories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${categories.length}`);
+categories.forEach((el) => {
+  console.log(el.firstElementChild.textContent);
+  console.log(el.lastElementChild.childElementCount);
+});
